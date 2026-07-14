ce805f7e1532af5a6f5f29ca8ef182b3990063d3
laminetallth <laminetall.th@gmail.com>
2026-07-14 15:10:58 +0200

    Aggiunta email admin ai ruoli

--- utenti-ruoli.js
+++ utenti-ruoli.js
@@ -2,6 +2,7 @@
 // Non inserire password in questo file: le password sono gestite solo da Firebase Authentication.
 window.USER_ROLES = {
   "laminetall.th@gmail.com": "admin",
+  "laminetall.th+admin@gmail.com": "admin",
   "venditore1@email.com": "venditore",
   "venditore2@email.com": "venditore"
 };
