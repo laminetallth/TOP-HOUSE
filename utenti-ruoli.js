e860ae01c1dde22af929604c62d1b3c2c8d197aa
laminetallth <laminetall.th@gmail.com>
2026-09-03 10:50:07 +0200

    Aggiunto Antonio Attardi come venditore

--- utenti-ruoli.js
+++ utenti-ruoli.js
@@ -6,5 +6,6 @@ window.USER_ROLES = {
   "gabrielestraniero.th+admin@gmail.com": "admin",
   "venditore1@email.com": "venditore",
   "venditore2@email.com": "venditore",
-  "isabelladattoli1@gmail.com": "venditore"
+  "isabelladattoli1@gmail.com": "venditore",
+  "antonioattardi.th@gmail.com": "venditore"
 };
