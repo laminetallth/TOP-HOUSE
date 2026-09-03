37df5cee84280c7ad5fbf43b1b1d3884ac94b530
laminetallth <laminetall.th@gmail.com>
2026-09-03 11:56:26 +0200

    Aggiunge loader gestione PDF

---
+++ gestione-documenti-loader.js
@@ -0,0 +1,7 @@
+// Caricatore della gestione PDF nelle cartelle TOP HOUSE.
+(() => {
+  const script = document.createElement('script');
+  script.src = 'gestione-documenti.js?v=' + Date.now();
+  script.defer = true;
+  document.head.appendChild(script);
+})();
