734e1095a6ba4a9516fe3b6e0e743f24f40a51c3
laminetallth <laminetall.th@gmail.com>
2026-09-15 09:11:02 +0200

    Aggiunto info@solarity-energysolutions.com come admin

--- utenti-ruoli.js
+++ utenti-ruoli.js
@@ -4,6 +4,7 @@ window.USER_ROLES = {
   "laminetall.th@gmail.com": "admin",
   "laminetall.th+admin@gmail.com": "admin",
   "gabrielestraniero.th+admin@gmail.com": "admin",
+  "info@solarity-energysolutions.com": "admin",
   "venditore1@email.com": "venditore",
   "venditore2@email.com": "venditore",
   "isabelladattoli1@gmail.com": "admin",
